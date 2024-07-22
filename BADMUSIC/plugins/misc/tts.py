a1e6bc92aea714fabf8a4a42564eed61dc2c3c60
𓆩𝄟≛⃝ 𝐜𝐮𝐭𝐞𓆩Devil𓆪🦋≛⃝king𝄟𓆪 <166118712+CuteDevilKing@users.noreply.github.com>
2024-07-22 15:27:21 +0530

    Update tts.py

--- BADMUSIC/plugins/misc/tts.py
+++ BADMUSIC/plugins/misc/tts.py
@@ -8,5 +8,5 @@ def text_to_speech(client, message):
     text = message.text.split(' ', 1)[1]
     tts = gTTS(text=text, lang='hi')
     tts.save('ᴮᴬᴰ ᴬᵁᴰᴵᴼ.mp3')
-    client.send_audio(message.chat.id, 'ᴮᴬᴰ ᴬᵁᴰᴵᴼ.mp3')
+    client.send_audio(message.chat.id, 'Dᴇᴠɪʟ GᴜʀᴜJɪ.mp3')
 

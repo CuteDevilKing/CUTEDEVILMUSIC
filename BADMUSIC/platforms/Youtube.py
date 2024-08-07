d6624ca7abb8012d7b073a38239d977c7285e73d
𓆩𝄟≛⃝ 𝐜𝐮𝐭𝐞𓆩Devil𓆪🦋≛⃝king𝄟𓆪 <166118712+CuteDevilKing@users.noreply.github.com>
2024-08-07 12:05:47 +0530

    Update Youtube.py

--- BADMUSIC/platforms/Youtube.py
+++ BADMUSIC/platforms/Youtube.py
@@ -34,7 +34,8 @@ class YouTubeAPI:
         self.status = "https://www.youtube.com/oembed?url="
         self.listbase = "https://youtube.com/playlist?list="
         self.reg = re.compile(r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])")
-
+    
+    
     async def exists(self, link: str, videoid: Union[bool, str] = None):
         if videoid:
             link = self.base + link
